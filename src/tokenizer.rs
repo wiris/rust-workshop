15409bf9ef0f95fb621e71bb2c1a378a55443cb8
francesc <francesc@wiris.com>
2022-04-25 12:45:01 +0200

    feat: Hint for the first excercise

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -3,12 +3,26 @@ const LATEX_COMMANDS : [&str; 4] = ["\\frac", "\\sqrt", "\\pm" , "\\text"];
 pub fn tokenize(latex: &str) -> Vec<String> {
 
     // Mutable empty string where the result should be built on
-    let mut result = Vec::new();
+    let mut result: Vec<String> = Vec::new();
 
+    let mut inside_text = false;
 
     // For each char on the imput
     for c in latex.chars() {
-        // do something
+        match c {
+            '\\' => {
+                // Command prefix. Read command? Append it to `result`? Update `inside_text`?
+            },
+            '}' => {
+                // Closing bracket. Apend it to `result`? Update `inside_text`?
+            }
+            ' ' => { 
+                // Blank soace. Apend it to `result`?
+            },
+            _ => {
+                // Any other character. Apend it to `result`?
+            }
+        }
     }
 
     return result;
@@ -45,7 +59,7 @@ mod test {
 
     #[test]
     fn tokenize_quadratic_formula() {
-        check("x \\text{ equals } \\frac{-b \\pm \\sqrt{b^2 - 4ac}}{2a}", 
-        "x \\text {   e q u a l s   } \\frac { - b \\pm \\sqrt { b ^ 2 - 4 a c } } { 2 a }");
+        check("x \\text{ equals } \\frac{-b \\pm \\sqrt{b^2 - 4ac}}{2a}", 
+        "x \\text {   e q u a l s   } \\frac { - b \\pm \\sqrt { b ^ 2 - 4 a c } } { 2 a }");
     }
 }
